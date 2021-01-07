8ae6eea161ede749ebda7be41e0099c68054c212
corynezin <cory.nezin@gmail.com>
2021-01-06 22:42:59 -0500

    add backend

--- backend/src/__main__.py
+++ backend/src/__main__.py
@@ -5,17 +5,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'helloworld'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+game = {}
+
 @app.route('/user')
 def user():
     return '"brenda"'
 
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
-
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
+@socketio.on('guess_submitted')
+def guess_submitted(json, methods=['GET', 'PUT', 'POST']):
+    socketio.emit('guess_received', json)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
