afa92f72209aab63c2613c262445b60a0b27988a
Trinity Hall <trinityhall2021@gmail.com>
2021-01-06 22:40:07 -0500

    added intercept code

--- backend/src/__main__.py
+++ backend/src/__main__.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'helloworld'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+
 @app.route('/user')
 def user():
     return '"brenda"'
@@ -16,6 +17,11 @@ def messageReceived(methods=['GET', 'POST']):
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
+
+@socketio.on('interceptSubmitted')
+def interceptSubmitted(json, methods=['GET', 'POST']):
+    print("Received intercept: " + str(json))
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
