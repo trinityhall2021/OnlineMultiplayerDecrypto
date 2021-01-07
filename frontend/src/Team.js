35fb5714a35655e15feeba313244d926299ebbec
corynezin <cory.nezin@gmail.com>
2021-01-06 22:17:46 -0500

    convert team to function

--- frontend/src/Team.js
+++ frontend/src/Team.js
@@ -1,31 +1,19 @@
 import React, { Component } from "react";
 
-class Team extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      team_name: props.team_name,
-      team_players: props.team_players,
-      num_misses: props.num_misses,
-      num_intercepts: props.num_intercepts,
-    };
-  }
+function Team(props) {
+  const team_players = [];
 
-  render() {
-    const team_players = [];
-
-    for (let i = 0; i < this.state.team_players.length; i++) {
-      team_players.push(<p>{this.state.team_players[i]}</p>);
-    }
-    return (
-      <div>
-        <p> {this.state.team_name} Team </p>
-        {team_players}
-        <p># misses: {this.state.num_misses}</p>
-        <p># intercepts: {this.state.num_intercepts}</p>
-      </div>
-    );
+  for (let i = 0; i < props.team_players.length; i++) {
+    team_players.push(<p>{props.team_players[i]}</p>);
   }
+  return (
+    <div>
+      <p> {props.team_name} Team </p>
+      {team_players}
+      <p># misses: {props.num_misses}</p>
+      <p># intercepts: {props.num_intercepts}</p>
+    </div>
+  );
 }
 
 export default Team;
