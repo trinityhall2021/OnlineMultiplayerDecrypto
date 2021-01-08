4a718db09b110a53d0d254b9512f76218c6f3782
corynezin <cory.nezin@gmail.com>
2021-01-07 22:43:01 -0500

    add default room id

--- frontend/src/Socket.js
+++ frontend/src/Socket.js
@@ -1,6 +1,8 @@
 import socketIOClient from "socket.io-client";
 
 const ENDPOINT = "http://127.0.0.1:5000";
-const socket = socketIOClient(ENDPOINT);
+const socket = socketIOClient(
+  ENDPOINT,
+  {query: {room_id: 'main'}});
 
 export default socket;
