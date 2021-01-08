75b363720509ce63471b06e9d85fd48a4f4ab967
Trinity Hall <trinityhall2021@gmail.com>
2021-01-07 22:53:45 -0500

    added cutive mono font

--- frontend/src/LandingPage.js
+++ frontend/src/LandingPage.js
@@ -5,7 +5,7 @@ import history from './history'
 
 function submitNameAndJoinGame(name) {
     console.log(name);
-    history.push('/game')
+    history.push('/game');
     socket.emit("submit_name", name );
 }
 
@@ -13,6 +13,8 @@ class LandingPage extends Component {
   render() {
     return (
         <div>
+        <link href="https://fonts.googleapis.com/css2?family=Cutive+Mono&display=swap" rel="stylesheet" />          
+        <h1>Decrypto</h1>
             <input type="text" id="fname" name="fname" />
             <button onClick={() => 
                 submitNameAndJoinGame(document.getElementById("fname").value)}>
