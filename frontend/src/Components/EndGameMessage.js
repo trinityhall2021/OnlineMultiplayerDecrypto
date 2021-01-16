d95fb40fcc14b69f6e608a2d28b722c545f26d60
Trinity Hall <trinityhall2021@gmail.com>
2021-01-15 23:11:40 -0500

    added end game message class

---
+++ frontend/src/Components/EndGameMessage.js
@@ -0,0 +1,39 @@
+import { Alert, Grid, Button, Header, Table } from "tabler-react";
+
+const EndGameMessage = (props) => {
+
+
+    if (props.red_team_endgame === "not yet") {
+        return <Alert />
+    }
+
+    const endgameColor = props.red_team_endgame === "win" ? (
+        "danger"
+    ) : props.red_team_endgame === "loss" ? (
+        "primary"
+    ) : (
+        "warning"
+    ) 
+
+    const endGameMessage = props.red_team_endgame === "win" ? (
+        "Red team wins!"
+    ) : props.red_team_endgame === "loss" ? (
+        "Blue team wins!"
+    ) : props.red_team_endgame === "tie" ? (
+        "It is a tie!"
+    ) : (
+        "Game hasn't ended yet"
+    );
+
+    return (
+        <Grid.Row className="px-5 mx-5">
+        <Grid.Col>
+        <Alert type={endgameColor} align="center" isDismissible>
+            <strong>{endGameMessage}</strong>
+        </Alert> 
+        </Grid.Col>
+        </Grid.Row>
+        )
+};
+
+export default EndGameMessage;
